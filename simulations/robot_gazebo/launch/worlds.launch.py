464e7d2cd516a517f8fba215103c9e26840eef55
Boyu Jia <kukd8meop@gmail.com>
2026-05-07 23:03:49 +0800

    移植官方gazebo仿真包，并删除幽灵依赖包

---
+++ simulations/robot_gazebo/launch/worlds.launch.py
@@ -0,0 +1,84 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription,LaunchService
+from launch.actions import DeclareLaunchArgument,OpaqueFunction
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+
+
+def launch_setup(context):
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true').perform(context)
+    world_name = LaunchConfiguration('world_name', default='empty').perform(context)
+    nav = LaunchConfiguration('nav', default='false').perform(context)
+    moveit_unite = LaunchConfiguration('moveit_unite', default='false').perform(context)
+    machine_type = LaunchConfiguration('machine_type', default='ROSOrin_Mecanum').perform(context)
+
+
+    moveit_unite_arg = DeclareLaunchArgument('moveit_unite', default_value=moveit_unite)
+    machine_type_arg = DeclareLaunchArgument('machine_type', default_value=machine_type)
+    nav_arg = DeclareLaunchArgument('nav',default_value=nav)
+    use_sim_time_arg = DeclareLaunchArgument('use_sim_time',default_value=use_sim_time)
+    world_name_arg = DeclareLaunchArgument('world_name',default_value=world_name)
+
+
+    robot_gazebo_path = get_package_share_directory('robot_gazebo')
+
+
+    # world
+    world = os.path.join(robot_gazebo_path,"worlds", world_name+".sdf")
+    gz_sim = IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [os.path.join(get_package_share_directory('ros_gz_sim'),
+                'launch', 'gz_sim.launch.py')]),
+                launch_arguments=[('ign_args', [' -r ' + world])])
+
+    ros_gz_bridge_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(robot_gazebo_path, 'launch/ros_gz_bridge.launch.py')
+            ),
+        launch_arguments={
+            'use_sim_time': use_sim_time,
+        }.items(),
+    )
+
+    spwan_model_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(robot_gazebo_path, 'launch/spwan_model.launch.py')
+            ),
+        launch_arguments={
+            'moveit_unite': moveit_unite,
+            'world_name': world_name,
+            'use_sim_time': use_sim_time,
+        }.items(),
+    )
+
+
+    return ([
+        use_sim_time_arg,
+        world_name_arg,
+        nav_arg,
+        moveit_unite_arg,
+        machine_type_arg,
+        SetEnvironmentVariable('MACHINE_TYPE', machine_type),
+        gz_sim,
+        spwan_model_launch,
+        ros_gz_bridge_launch,
+    ])
+    
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        OpaqueFunction(function = launch_setup)
+    ])
+
+
+
+if __name__ == '__main__':
+    ld = generate_launch_description()
+
+    ls = LaunchService()
+    ls.include_launch_description(ld)
+    ls.run()
